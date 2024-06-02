d5c58cae6672b9de2a570f7669bd0fb7f9b4b167
Rujan <grimr0833@gmail.com>
2024-06-02 11:43:21 +0545

    2nd

--- lab2/q2.cpp
+++ lab2/q2.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort(int arr[], int n, int& total_steps) {
+void selectionSort(int arr[], int n, int& total_steps) {
     total_steps = 0;
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+    for (int i = 0; i < n-1; i++) {
+        int min_idx = i;
+        for (int j = i+1; j < n; j++) {
             total_steps++;
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
         }
 
-        arr[j + 1] = key;
-        total_steps++;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+        total_steps += 3;  // Steps for swapping
     }
 }
 
@@ -30,7 +31,7 @@ int main() {
     }
 
     int total_steps;
-    insertionSort(arr, n, total_steps);
+    selectionSort(arr, n, total_steps);
 
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
